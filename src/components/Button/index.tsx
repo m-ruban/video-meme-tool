b83a8732958c4d02e232d25214b5992d440f40ce
Ruban Mikhail <m.ruban@hh.ru>
2025-05-04 19:09:02 +0200

    add button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode, ButtonHTMLAttributes, MouseEvent } from "react";
+
+import 'src/components/Button/button.less';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+    children: ReactNode;
+};
+
+const Button = ({onClick, children, ...props}: ButtonProps) => {
+    return (
+      <button className="button" onClick={onClick} {...props}>
+        {children}
+      </button>
+    );
+}
+
+export { Button };
