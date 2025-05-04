668cc6d55e6179b136dc0cf12651638f3927c575
Ruban Mikhail <m.ruban@hh.ru>
2025-05-04 19:09:22 +0200

    add loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,19 @@
+import { motion } from 'framer-motion';
+
+import { Loading } from "src/components/Icon/Loading";
+
+import 'src/components/Loader/loader.less';
+
+const Loader = () => {
+  return (
+    <motion.div
+      className="loader"
+      animate={{ rotate: 360 }}
+      transition={{ repeat: Infinity, duration: 1.5, ease: 'circInOut' }}
+    >
+      <Loading />
+    </motion.div>
+);
+}
+
+export { Loader };
