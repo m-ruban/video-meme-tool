e2a03844acabb24ef1f02674194be93063418f31
Ruban Mikhail <m.ruban@hh.ru>
2025-05-04 19:08:51 +0200

    add main arrows

---
+++ src/components/Arrow/ArrowWrapper.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+import 'src/components/Arrow/arrow-wrapper.less';
+
+type ArrowProps = {
+    children: ReactNode;
+};
+
+const ArrowWrapper = ({children}: ArrowProps) => {
+    return (
+      <div className="arrow-wrapper">
+        {children}
+      </div>
+    );
+}
+
+export { ArrowWrapper };
