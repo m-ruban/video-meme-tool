7d6fbf6cb257d678d2f6742a3cbfcd069046bda4
Ruban Mikhail <m.ruban@hh.ru>
2025-05-04 19:04:57 +0200

    add trls

---
+++ src/lang/ru.tsx
@@ -0,0 +1,17 @@
+const ru = {
+    appTitle: 'Video Meme Tool',
+    step1: () => <>Загрузи<br />видео</>,
+    step2: 'Редактируй ролик',
+    step3: 'Опубликуй результат',
+    uploadTitle: 'Перетащите или выберите файл',
+    uploadDescription: 'Размер файла - 5 МБ. Формат - MP4',
+    uploadButton: 'Выберите файл',
+    uploadAlert: 'Загружая видеофайл, пользователь подтверждает наличие прав на его использование',
+    uploadFileSize: ({size}: {size: string}) => <>Размер файла {size} МБ</>,
+    uploadFileRestriction: 'Разрешены только .mp4 видеофайлы',
+    uploadButtonCancel: 'Отмена',
+    uploadFileLoading: 'Файл загружается...',
+    
+}
+
+export { ru };
---
+++ src/lang/trls.ts
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+import { ru } from 'src/lang/ru';
+
+type TrlKey = keyof typeof ru;
+
+const getTrl = (key: TrlKey, params?: any): ReactNode => {
+    const trl = ru[key];
+    if (typeof trl === "function") {
+        return trl(params);
+    }
+    return trl;
+}
+
+export { getTrl }
