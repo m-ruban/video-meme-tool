3104ef016abcd511c47b23c97ef2a94468896078
Ruban Mikhail <m.ruban@hh.ru>
2025-05-18 23:54:53 +0200

    config store

---
+++ src/dummy.ts
@@ -0,0 +1,38 @@
+const dummyContent = new Blob(['Hello, world!'], { type: 'text/plain' });
+const dummyFile = new File([dummyContent], 'dummy.txt', {
+  type: 'text/plain',
+  lastModified: Date.now(),
+});
+
+export const meme = {
+  file: dummyFile,
+  base: 'fwvPxGccNj-1746300506542',
+  ext: '.mp4',
+  name: 'fwvPxGccNj-1746300506542.mp4',
+  link: '/video/2025-05-03/fwvPxGccNj-1746300506542.mp4',
+  duration: 21.078,
+  audio: '/video/2025-05-03/fwvPxGccNj-1746300506542/audio.aac',
+  frames: [
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-001.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-002.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-003.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-004.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-005.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-006.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-007.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-008.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-009.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-010.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-011.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-012.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-013.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-014.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-015.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-016.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-017.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-018.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-019.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-020.png',
+    '/video/2025-05-03/fwvPxGccNj-1746300506542/frame-021.png',
+  ],
+};
--- src/store.ts
+++ src/store.ts
@@ -1,33 +1,43 @@
 import { create } from 'zustand';
 
-type TokenState = string;
-type TokenAction = { type: 'token/set', payload: string };
+import { meme } from 'src/dummy';
 
-interface Meme {
+type TokenState = string;
+type TokenAction = { type: 'token/set'; payload: string };
+
+export interface Meme {
   file: File;
   /** base file name */
-  base: string,
+  base: string;
   /** ext of file */
-  ext: string,
+  ext: string;
   /** full file name */
-  name: string,
+  name: string;
   /** full link to file */
-  link: string,
+  link: string;
   /** in sec */
-  duration: number,
+  duration: number;
   /** full link to audio */
-  audio: string,
+  audio: string;
   frames: string[];
 }
-type NullableMeme = Meme | null;
-type MemeAction = { type: 'meme/set', payload: Meme };
+export type NullableMeme = Meme | null;
+type MemeAction = { type: 'meme/set'; payload: Meme };
+
+type StepState = 'load-file' | 'edit-file' | 'view-file';
+type StepAction = { type: 'step/set'; payload: StepState };
+
+type VideoLoaded = boolean;
+type VideoLoadedAction = { type: 'meme-loaded/set'; payload: boolean };
 
 type AppState = {
   token: TokenState;
   meme: NullableMeme;
+  step: StepState;
+  videoLoaded: VideoLoaded;
 };
 
-type AppAction = TokenAction | MemeAction;
+type AppAction = TokenAction | MemeAction | StepAction | VideoLoadedAction;
 
 interface Store {
   state: AppState;
@@ -52,15 +62,38 @@ const memeReducer = (state: NullableMeme, action: AppAction): NullableMeme => {
   }
 };
 
+const stepReducer = (state: StepState, action: AppAction): StepState => {
+  switch (action.type) {
+    case 'step/set':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const videoLoadedReducer = (state: VideoLoaded, action: AppAction): VideoLoaded => {
+  switch (action.type) {
+    case 'meme-loaded/set':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
 export const useAppStore = create<Store>((set) => ({
   state: {
     token: '',
-    meme: null,
+    meme: meme,
+    step: 'edit-file',
+    videoLoaded: false,
   },
-  dispatch: (action) => set((store) => ({
-    state: {
-      token: tokenReducer(store.state.token, action),
-      meme: memeReducer(store.state.meme, action),
-    },
-  })),
+  dispatch: (action) =>
+    set((store) => ({
+      state: {
+        token: tokenReducer(store.state.token, action),
+        meme: memeReducer(store.state.meme, action),
+        step: stepReducer(store.state.step, action),
+        videoLoaded: videoLoadedReducer(store.state.videoLoaded, action),
+      },
+    })),
 }));
