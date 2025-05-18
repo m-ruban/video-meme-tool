889d544fef64a4ac9a9c02e5cc060b8932af9d56
Ruban Mikhail <m.ruban@hh.ru>
2025-05-18 23:59:54 +0200

    add upload

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,12 +1,13 @@
 import { useState, useCallback } from 'react';
 import { useDropzone, FileRejection } from 'react-dropzone';
 
-import { VideoFile } from "src/components/Icon/VideoFile";
-import { Error } from "src/components/Icon/Error";
-import { Button } from "src/components/Button";
-import { Loader } from "src/components/Loader";
-import { fetcher } from "src/api/fetcher";
-import { getTrl } from "src/lang/trls";
+import { VideoFile } from 'src/components/Icon/VideoFile';
+import { Error } from 'src/components/Icon/Error';
+import { Button } from 'src/components/Button';
+import { Loader } from 'src/components/Loader';
+import { fetcher } from 'src/api/fetcher';
+import { Meme, useAppStore } from 'src/store';
+import { getTrl } from 'src/lang/trls';
 
 import 'src/components/Upload/upload.less';
 
@@ -25,32 +26,38 @@ const Upload = () => {
   const [uploadedFile, setUploadedFile] = useState<UploadedFile>();
   const [error, setError] = useState('');
   const fileSize = uploadedFile?.file.size || 0;
+  const { dispatch } = useAppStore();
 
   const onDropRejected = useCallback((fileRejections: FileRejection[]) => {
     if (fileRejections.length === 1) {
       const rejectedFile = fileRejections[0];
-      setUploadedFile({ file: rejectedFile.file, loading: false })
+      setUploadedFile({ file: rejectedFile.file, loading: false });
       setError(rejectedFile.errors[0].message);
     }
   }, []);
 
-  const onDropAccepted = useCallback(async (files: File[]) => {
-    if (files.length === 1) {
-      const file = files[0];
-      setUploadedFile({ file, loading: true })
-      setError('');
+  const onDropAccepted = useCallback(
+    async (files: File[]) => {
+      if (files.length === 1) {
+        const file = files[0];
+        setUploadedFile({ file, loading: true });
+        setError('');
 
-      const formData = new FormData();
-      formData.append('file', file);
+        const formData = new FormData();
+        formData.append('file', file);
 
-      const response = await fetcher.post('/api/v1/video/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      console.log(response.data);
-    }
-  }, []);
+        const response = await fetcher.post<Meme>('/api/v1/video/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        dispatch({ type: 'meme/set', payload: { ...response.data, file } });
+        dispatch({ type: 'step/set', payload: 'edit-file' });
+      }
+    },
+    [dispatch]
+  );
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: IMAGE_ACCEPT,
@@ -66,24 +73,21 @@ const Upload = () => {
         <input {...getInputProps()} />
         <div className="upload-icon">
           {error && <Error />}
-          {!error && 
-            <>
-              {uploadedFile?.loading ? <Loader />  : <VideoFile />}
-            </>
-          }
+          {!error && <>{uploadedFile?.loading ? <Loader /> : <VideoFile />}</>}
         </div>
         <div className="upload-title">
           {error && getTrl('uploadFileRestriction')}
-          {!error && <>{uploadedFile?.loading ? getTrl('uploadFileLoading') : getTrl('uploadTitle')}</>}
+          {!error && (
+            <>{uploadedFile?.loading ? getTrl('uploadFileLoading') : getTrl('uploadTitle')}</>
+          )}
         </div>
         <div className="upload-description">
-          {(error || uploadedFile) ? 
-            getTrl('uploadFileSize', { size: sizeMB(fileSize) }) : 
-            getTrl('uploadDescription')
-          }
+          {error || uploadedFile
+            ? getTrl('uploadFileSize', { size: sizeMB(fileSize) })
+            : getTrl('uploadDescription')}
         </div>
         <div>
-          {(error || uploadedFile) ? 
+          {error || uploadedFile ? (
             <Button
               onClick={(event) => {
                 setUploadedFile(undefined);
@@ -92,14 +96,15 @@ const Upload = () => {
               }}
             >
               {getTrl('uploadButtonCancel')}
-            </Button> : 
+            </Button>
+          ) : (
             <Button onClick={() => null}>{getTrl('uploadButton')}</Button>
-          }
+          )}
         </div>
       </div>
       <div className="upload-alert">{getTrl('uploadAlert')}</div>
     </div>
-    );
-}
+  );
+};
 
 export { Upload };
