332fd46c556c77625d0b969e014c8504747a03cb
Ruban Mikhail <m.ruban@hh.ru>
2025-07-26 23:27:30 +0200

    clear console log

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -5,7 +5,6 @@ const fetcher = axios.create();
 
 fetcher.interceptors.request.use((config) => {
   const token = useAppStore.getState().state.token;
-  console.log('token = ', token);
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
