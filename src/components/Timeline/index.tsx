2091952e8bb071cf6cb3b67486a1c0e0d336d44e
Ruban Mikhail <m.ruban@hh.ru>
2025-07-26 23:42:54 +0200

    add shadows

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,4 +1,6 @@
-import { Meme } from 'src/store';
+import { useRef, useCallback, MouseEvent, useEffect } from 'react';
+
+import { Meme, useAppStore } from 'src/store';
 import { Playhead } from 'src/components/Timeline/components/Playhead';
 import { Ruler } from 'src/components/Timeline/components/Ruler';
 import { VideoTrack } from 'src/components/Timeline/components/VideoTrack';
@@ -10,15 +12,67 @@ interface TimelineProps {
   meme: Meme;
 }
 
+const updateShadows = (timeline: HTMLDivElement, timelineScrollableDiv: HTMLDivElement) => {
+  const scrollLeft = timelineScrollableDiv.scrollLeft;
+  const scrollWidth = timelineScrollableDiv.scrollWidth;
+  const clientWidth = timelineScrollableDiv.clientWidth;
+  timeline.style.setProperty('--shadow-left', scrollLeft > 0 ? '1' : '0');
+  timeline.style.setProperty('--shadow-right', scrollLeft + clientWidth < scrollWidth ? '1' : '0');
+};
+
 const Timeline = ({ meme }: TimelineProps) => {
+  const dispatch = useAppStore((store) => store.dispatch);
+  const playerInstance = useAppStore((store) => store.state.playerInstance);
+  const rulerRef = useRef<HTMLDivElement>(null);
+  const timelineRef = useRef<HTMLDivElement>(null);
+  const timelineScrollableRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!timelineRef.current || !timelineScrollableRef.current) {
+      return;
+    }
+    const timelineDiv = timelineRef.current;
+    const timelineScrollableDiv = timelineScrollableRef.current;
+    const updateShadowsOnTimeline = () => {
+      updateShadows(timelineDiv, timelineScrollableDiv);
+    };
+
+    updateShadowsOnTimeline();
+    timelineScrollableDiv.addEventListener('scroll', updateShadowsOnTimeline);
+    window.addEventListener('resize', updateShadowsOnTimeline);
+
+    return () => {
+      timelineScrollableDiv.removeEventListener('scroll', updateShadowsOnTimeline);
+      window.removeEventListener('resize', updateShadowsOnTimeline);
+    };
+  }, []);
+
+  const handleClick = useCallback(
+    (event: MouseEvent<HTMLDivElement>) => {
+      if (!timelineScrollableRef.current || !playerInstance) {
+        return;
+      }
+      const timelineScrollableDiv = timelineScrollableRef.current;
+      const timelineScrollableRect = timelineScrollableDiv.getBoundingClientRect();
+      const clickX =
+        event.clientX - timelineScrollableRect.left + timelineScrollableRef.current.scrollLeft;
+      const percentNumber = clickX / timelineScrollableRef.current.scrollWidth; // 0.XXXXXX
+      const percentRaw = percentNumber * 100; // XX.XXXX
+      const percent = Math.min(100, Math.max(0, Math.round(percentRaw * 10) / 10)); // ~XX.X
+      dispatch({ type: 'played-percent/set', payload: percent });
+      playerInstance.seekTo(meme.duration * percentNumber, 'seconds');
+    },
+    [dispatch, playerInstance, meme]
+  );
+
   return (
     <div className="timeline-wrapper">
-      <div className="timeline">
-        <div className="timeline-scrollable">
-          <Ruler duration={meme.duration} />
+      <div ref={timelineRef} className="timeline">
+        <div ref={timelineScrollableRef} className="timeline-scrollable" onClick={handleClick}>
+          <Ruler ref={rulerRef} duration={meme.duration} />
           <VideoTrack frames={meme.frames} />
           <Waveform path={meme.waveform} />
-          <Playhead />
+          <Playhead rulerRef={rulerRef} />
         </div>
       </div>
     </div>
