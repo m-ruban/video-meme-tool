6d02a149528322f5992982422dad23450149ce5c
Ruban Mikhail <m.ruban@hh.ru>
2025-07-27 23:53:48 +0200

    add selection on waveform

--- src/components/Timeline/components/Waveform/index.tsx
+++ src/components/Timeline/components/Waveform/index.tsx
@@ -1,13 +1,95 @@
+import { useRef, useCallback, MouseEventHandler, useEffect } from 'react';
 import 'src/components/Timeline/components/Waveform/wave-form.less';
 
 interface WaveformProps {
   path: string;
 }
 
+interface Position {
+  x: number;
+  y: number;
+}
+
+const cancelEvent: MouseEventHandler<HTMLDivElement> = (event) => event.stopPropagation();
+
 export const Waveform = ({ path }: WaveformProps) => {
+  const selectionLayerRef = useRef<HTMLDivElement>(null);
+  const selectionRef = useRef<HTMLDivElement>(null);
+  const startPosRef = useRef<Position | null>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  const handleMouseMove = useCallback((event: MouseEvent) => {
+    if (!startPosRef.current || !imgRef.current || !selectionRef.current) {
+      return;
+    }
+    const startPos = startPosRef.current;
+    const rect = imgRef.current.getBoundingClientRect();
+    const currentX = event.clientX - rect.left;
+    const x = Math.min(currentX, startPos.x);
+    const width = Math.abs(currentX - startPos.x);
+    selectionRef.current.style.left = `${x}px`;
+    selectionRef.current.style.width = `${width}px`;
+  }, []);
+
+  const handleMouseUp = useCallback(() => {
+    window.removeEventListener('mousemove', handleMouseMove);
+    window.removeEventListener('mouseup', handleMouseUp);
+    startPosRef.current = null;
+
+    setTimeout(() => {
+      if (!selectionRef.current) {
+        return;
+      }
+      selectionRef.current.style.left = `0px`;
+      selectionRef.current.style.width = `0px`;
+    }, 200);
+  }, [handleMouseMove]);
+
+  const handleMouseDown: MouseEventHandler<HTMLDivElement> = useCallback(
+    (event) => {
+      if (!imgRef.current || !selectionRef.current) {
+        return;
+      }
+      event.stopPropagation();
+      const rect = imgRef.current.getBoundingClientRect();
+      const startX = event.clientX - rect.left;
+      const startY = event.clientY - rect.top;
+      startPosRef.current = { x: startX, y: startY };
+      selectionRef.current.style.left = `${startX}px`;
+      selectionRef.current.style.width = `0px`;
+      window.addEventListener('mousemove', handleMouseMove);
+      window.addEventListener('mouseup', handleMouseUp);
+    },
+    [handleMouseMove, handleMouseUp]
+  );
+
+  useEffect(() => {
+    if (!imgRef.current || !selectionLayerRef.current) {
+      return;
+    }
+    const updateBarWidth = () => {
+      if (!imgRef.current || !selectionLayerRef.current) {
+        return;
+      }
+      selectionLayerRef.current.style.width = imgRef.current.scrollWidth + 'px';
+    };
+    const resizeObserver = new ResizeObserver(updateBarWidth);
+    resizeObserver.observe(imgRef.current);
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
   return (
     <div className="waveform">
-      <img src={path} alt="Waveform" />
+      <img ref={imgRef} src={path} alt="Waveform" />
+      <div
+        ref={selectionLayerRef}
+        className="waveform-selection-layer"
+        onMouseDown={handleMouseDown}
+        onClick={cancelEvent}
+      />
+      <div ref={selectionRef} className="waveform-selection-box" />
     </div>
   );
 };
