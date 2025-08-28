0d7c97329d38509e195fb9d8fc6f809cc78b0577
Ruban Mikhail <m.ruban@hh.ru>
2025-08-29 00:29:19 +0200

    add api token into editor

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -2,10 +2,12 @@ import { useAppStore } from 'src/store';
 import { Player } from 'src/components/Player';
 import { Timeline } from 'src/components/Timeline';
 import { SaveButton } from 'src/components/SaveButton';
+import { useApiToken } from 'src/api/useApiToken';
 
 const Editor = () => {
   const meme = useAppStore((store) => store.state.meme);
   const videoLoaded = useAppStore((store) => store.state.videoLoaded);
+  useApiToken();
   if (!meme) {
     return;
   }
