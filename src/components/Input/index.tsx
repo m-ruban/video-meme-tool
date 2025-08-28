6a518bcf279ace26491166b6d3c63e9b295c7fdf
Ruban Mikhail <m.ruban@hh.ru>
2025-08-29 00:31:59 +0200

    add ref into input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,22 +1,22 @@
-import { FC, ChangeEvent, ComponentPropsWithoutRef } from 'react';
+import { forwardRef, ChangeEvent, ComponentPropsWithoutRef } from 'react';
 import { Merge } from 'type-fest';
 import 'src/components/Input/input.less';
 
-export type InputProps = Merge<
-  ComponentPropsWithoutRef<'input'>,
-  {
-    value?: string | number;
-    placeholder?: string;
-    size?: number;
-    disabled?: boolean;
-    invalid?: boolean;
-    rounded?: boolean;
-    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-  }
->;
+interface InputHelper {
+  value?: string | number;
+  placeholder?: string;
+  size?: number;
+  disabled?: boolean;
+  invalid?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+}
 
-const Input: FC<InputProps> = ({ placeholder, rounded, ...inputProps }) => (
-  <input className={'input'} placeholder={placeholder} {...inputProps} />
-);
+export type InputProps = Merge<ComponentPropsWithoutRef<'input'>, InputHelper>;
+
+const Input = forwardRef<HTMLInputElement, InputProps>((inputProps, ref) => (
+  <input ref={ref} className={'input'} {...inputProps} />
+));
+
+Input.displayName = 'Input';
 
 export { Input };
