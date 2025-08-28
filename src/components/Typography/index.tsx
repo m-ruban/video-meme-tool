14c3372d0a37e2748b0953d80ea59709cb470d4b
Ruban Mikhail <m.ruban@hh.ru>
2025-08-29 00:30:47 +0200

    add typography props

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,13 +1,25 @@
 import { ReactNode } from 'react';
+import classnames from 'classnames';
 
 import 'src/components/Typography/typography.less';
 
+type Mode = 'primary' | 'secondary';
+type Weight = 'regular' | 'medium' | 'bold';
+
 interface TypographyProps {
   children: ReactNode;
+  mode?: Mode;
+  weight?: Weight;
 }
 
-const Typography = ({ children }: TypographyProps) => {
-  return <div className="typography">{children}</div>;
+const Typography = ({ children, mode = 'primary', weight = 'regular' }: TypographyProps) => {
+  return (
+    <div
+      className={classnames('typography', `typography_mode-${mode}`, `typography_weight-${weight}`)}
+    >
+      {children}
+    </div>
+  );
 };
 
 export { Typography };
