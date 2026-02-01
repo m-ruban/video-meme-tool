87db93edc0594fc7b860d595c018bb0097d4195e
Ruban Mikhail <m.ruban@hh.ru>
2026-02-01 20:59:53 +0100

    improve imput

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,16 @@
 import { forwardRef, ChangeEvent, ComponentPropsWithoutRef } from 'react';
 import { Merge } from 'type-fest';
+import classnames from 'classnames';
 import 'src/components/Input/input.less';
+
+type InputSize = 'small' | 'medium';
+type InputType = 'primary' | 'secondary';
 
 interface InputHelper {
   value?: string | number;
   placeholder?: string;
-  size?: number;
+  size?: InputSize;
+  inputType?: InputType;
   disabled?: boolean;
   invalid?: boolean;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
@@ -13,9 +18,15 @@ interface InputHelper {
 
 export type InputProps = Merge<ComponentPropsWithoutRef<'input'>, InputHelper>;
 
-const Input = forwardRef<HTMLInputElement, InputProps>((inputProps, ref) => (
-  <input ref={ref} className={'input'} {...inputProps} />
-));
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ size = 'small', inputType = 'primary', ...inputProps }, ref) => (
+    <input
+      ref={ref}
+      className={classnames('input', `input_size-${size}`, `input_type-${inputType}`)}
+      {...inputProps}
+    />
+  )
+);
 
 Input.displayName = 'Input';
 
