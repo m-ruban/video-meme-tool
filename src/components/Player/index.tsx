14447d086b8ee0bc3dac36bb6b51bb1d95090f45
Ruban Mikhail <m.ruban@hh.ru>
2026-02-01 21:01:14 +0100

    use save btn

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,7 +1,9 @@
-import { useRef, useState, useCallback, useEffect } from 'react';
+import { useRef, useState, useCallback, useEffect, type ReactNode, type FC } from 'react';
 import ReactPlayer from 'react-player';
 import classnames from 'classnames';
-import { Meme, useAppStore } from 'src/store';
+import { ParticallMeme, useAppStore } from 'src/store';
+import { Drop } from 'src/components/Drop';
+import { More } from 'src/components/Icon/More';
 import { PlayPause } from 'src/components/Icon/PlayPause';
 import { Volume } from 'src/components/Icon/Volume';
 import { ProgressBar } from 'src/components/ProgressBar';
@@ -9,14 +11,18 @@ import { ProgressBar } from 'src/components/ProgressBar';
 import 'src/components/Player/player.less';
 
 interface PlayerProps {
-  meme: Meme;
+  meme: ParticallMeme;
+  actions?: ReactNode;
 }
 
 const roundToPercent = (value: number) => Math.round(value * 1000) / 10;
 
 const DEFAULT_VOLUME = 0.8;
 
-const Player = ({ meme }: PlayerProps) => {
+const Player: FC<PlayerProps> = ({ meme, actions }) => {
+  const [showActions, setShowActions] = useState(false);
+  const activatorRef = useRef<HTMLDivElement>(null);
+  const handleShowActions = () => setShowActions((value) => !value);
   const dispatch = useAppStore((store) => store.dispatch);
   const videoLoaded = useAppStore((store) => store.state.videoLoaded);
   const playedPercent = useAppStore((store) => store.state.playedPercent);
@@ -74,16 +80,35 @@ const Player = ({ meme }: PlayerProps) => {
             <div className="player-duration-progress-bar">
               <ProgressBar value={playedPercent} onChange={handleProgressChange} />
             </div>
-            <div className="player-play" onClick={togglePlay}>
-              <PlayPause playing={playing} />
-            </div>
-            <div className="player-volume">
-              <div className="player-volume-icon">
-                <Volume />
+            <div className="player-actions-content">
+              <div className="player-play" onClick={togglePlay}>
+                <PlayPause playing={playing} />
               </div>
-              <div className="player-volume-progress-bar">
-                <ProgressBar value={volume * 100} onChange={handleVolumeChange} />
+              <div className="player-volume">
+                <div className="player-volume-icon">
+                  <Volume />
+                </div>
+                <div className="player-volume-progress-bar">
+                  <ProgressBar value={volume * 100} onChange={handleVolumeChange} />
+                </div>
               </div>
+              {actions && (
+                <>
+                  <div ref={activatorRef} className="player-right-actions">
+                    <More onClick={handleShowActions} />
+                  </div>
+                  <Drop
+                    show={showActions}
+                    activator={activatorRef}
+                    style={{ lineHeight: 0 }}
+                    withActivatorWidth={false}
+                    closeWithClickOutside
+                    onClose={handleShowActions}
+                  >
+                    {actions}
+                  </Drop>
+                </>
+              )}
             </div>
           </div>
         </div>
--- src/components/SaveButton/index.tsx
+++ src/components/SaveButton/index.tsx
@@ -1,17 +1,22 @@
+import { type FC } from 'react';
 import { Button } from 'src/components/Button';
 import { Save } from 'src/components/Icon/Save';
 
+import { useAppStore } from 'src/store';
+import { useSaveMeme } from 'src/api/useSaveMeme';
 import 'src/components/SaveButton/save-button.less';
 
-const SaveButton = () => {
+const SaveButton: FC = () => {
+  const meme = useAppStore((store) => store.state.meme);
+  const requestSaveMemeRequest = useSaveMeme();
+  const handleClickSaveMeme = () => {
+    requestSaveMemeRequest(meme?.link || '', (link) => {
+      window.location.href = link;
+    });
+  };
   return (
     <div className="save-button">
-      <Button
-        icon={<Save />}
-        onClick={() => {
-          console.log('stop');
-        }}
-      />
+      <Button icon={<Save />} onClick={handleClickSaveMeme} />
     </div>
   );
 };
